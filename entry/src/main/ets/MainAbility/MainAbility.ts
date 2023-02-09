22b53416d2308e721729b98ac108d9a37cc1a8ab
li-li-wang <wangliliang5@huawei.com>
2023-02-09 14:14:12 +0800

    Description:fix UI bug
    Match-id-5bf2027692404d4f3a43022da1c7ba36ace2cbc3

--- entry/src/main/ets/MainAbility/MainAbility.ts
+++ entry/src/main/ets/MainAbility/MainAbility.ts
@@ -1,9 +1,10 @@
 import UIAbility from '@ohos.app.ability.UIAbility'
 import datafile from '@ohos.file.fileAccess';
 import window from '@ohos.window';
+import abilityAccessCtrl, {Permissions} from '@ohos.abilityAccessCtrl'
 
 var TAG = "[DLPManager]"
-let permissionList: Array<string> = [
+let permissionList: Array<Permissions> = [
     "ohos.permission.READ_MEDIA",
     "ohos.permission.WRITE_MEDIA",
     "ohos.permission.FILE_ACCESS_MANAGER"
@@ -23,7 +24,14 @@ export default class MainAbility extends UIAbility {
 
     async onWindowStageCreate(windowStage) {
         console.log("[DLPManager] MainAbility onWindowStageCreate")
-        globalThis.context.requestPermissionsFromUser(permissionList)
+        var atManager = abilityAccessCtrl.createAtManager();
+        atManager.requestPermissionsFromUser(globalThis.context, permissionList).then((data) => {
+            console.info("[DLPManager] data:" + JSON.stringify(data));
+            console.info("[DLPManager] data permissions:" + data.permissions);
+            console.info("[DLPManager] data authResults:" + data.authResults);
+        }).catch((err) => {
+            console.info("[DLPManager] data:" + JSON.stringify(err));
+        })
         windowStage.setUIContent(this.context, "pages/index", null)
         windowStage.getMainWindow().then((win) => {
             win.setBackgroundColor("#00FFFFFF")
