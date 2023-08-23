c5a1a8a1941b4a3860bf6b7aa7ff371631f5d93f
li-li-wang <wangliliang5@huawei.com>
2023-08-23 17:33:44 +0800

    Description:viewAbility中onrequest超大函数整改
    Match-id-8011695f4782e6a662d41de9d005b66c1485e1b4

--- entry/src/main/ets/ViewAbility/ViewAbility.ts
+++ entry/src/main/ets/ViewAbility/ViewAbility.ts
@@ -56,6 +56,7 @@ export default class ViewAbility extends ServiceExtensionAbility {
   uri: string = '';
   file = null;
   stat = null;
+  accountInfo = null;
   uriInfo = null;
   linkUri: string = '';
   isCreated: boolean = false;
@@ -219,6 +220,224 @@ export default class ViewAbility extends ServiceExtensionAbility {
     }
   }
 
+  async getAccountAndOpenDLPFile(startId): Promise<void> {
+    hiTraceMeter.startTrace('DlpGetOsAccountJs', startId);
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.accountInfo = await getOsAccountInfo();
+        this.userId = await getUserId();
+      } catch (err) {
+        console.error(TAG, 'getOsAccountInfo failed', err.code, err.message);
+        hiTraceMeter.finishTrace('DlpGetOsAccountJs', startId);
+        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
+        opening = false;
+        await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_GET_ACCOUNT_ERROR });
+        fileio.closeSync(this.dlpFd);
+        reject(err); return;
+      }
+      hiTraceMeter.finishTrace('DlpGetOsAccountJs', startId);
+      if (!checkAccountLogin(this.accountInfo)) {
+        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
+        opening = false;
+        await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_NO_ACCOUNT_ERROR });
+        fileio.closeSync(this.dlpFd);
+        reject(); return;
+      }
+      hiTraceMeter.startTrace('DlpOpenDlpFileJs', startId);
+      try {
+        console.info(TAG, 'openDLPFile', this.fileName, this.dlpFd);
+        this.dlpFile = await dlpPermission.openDLPFile(this.dlpFd);
+      } catch (err) {
+        console.error(TAG, 'openDLPFile', this.fileName, 'failed', err.code, err.message);
+        try {
+          hiTraceMeter.finishTrace('DlpOpenDlpFileJs', startId);
+          hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
+          opening = false;
+          await this.sendDlpFileOpenFault(Constants.DLP_FILE_PARSE_ERROR, this.sandboxBundleName, -1, err.data); // 103:DLP_FILE_PARSE_ERROR
+          await startAlertAbility(globalThis.viewContext, err);
+        } catch (e) {
+          console.error(TAG, 'openDLPFile2', this.fileName, 'failed', e.code, e.message, e);
+        }
+        fileio.closeSync(this.dlpFd);
+        reject(); return;
+      }
+      hiTraceMeter.finishTrace('DlpOpenDlpFileJs', startId);
+      try {
+        await this.dlpGetAuthPerm();
+      } catch (err) {
+        reject(); return;
+      }
+      resolve();
+    });
+  }
+  async dlpGetAuthPerm(): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      if (globalThis.domainAccount) {
+        this.authPerm = getAuthPerm(this.accountInfo.domainInfo.accountName, this.dlpFile.dlpProperty);
+      } else {
+        this.authPerm = getAuthPerm(this.accountInfo.distributedInfo.name, this.dlpFile.dlpProperty);
+      }
+      if (this.authPerm < dlpPermission.DLPFileAccess.READ_ONLY ||
+        this.authPerm > dlpPermission.DLPFileAccess.FULL_CONTROL) {
+        opening = false;
+        await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_INSIDE_ERROR });
+        await this.closeFile();
+        reject();
+        return;
+      }
+      resolve();
+    });
+  }
+
+
+  async getPolicyAndInstallSandbox(startId): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      let sortByAuthPerm: boolean = false;
+      this.alreadyOpen = false;
+      try {
+        this.gatheringType = await dlpPermission.getDLPGatheringPolicy();
+        if (globalThis.fileOpenHistory[this.uri] !== undefined) {
+          console.info(TAG, 'file', this.fileName, 'already open');
+          this.appIndex = globalThis.fileOpenHistory[this.uri][Constants.FILE_OPEN_HISTORY_ONE];
+          this.linkFileName = globalThis.fileOpenHistory[this.uri][Constants.FILE_OPEN_HISTORY_TWO];
+          this.linkFd = globalThis.fileOpenHistory[this.uri][Constants.FILE_OPEN_HISTORY_THREE];
+          this.linkUri = globalThis.fileOpenHistory[this.uri][Constants.FILE_OPEN_HISTORY_FOUR];
+          await this.closeFile();
+          this.alreadyOpen = true;
+        }
+
+        if (globalThis.authPerm2Sandbox[this.authPerm] !== undefined &&
+          this.gatheringType === dlpPermission.GatheringPolicyType.GATHERING) {
+          this.appIndex = globalThis.authPerm2Sandbox[this.authPerm][1];
+          console.info(TAG, 'Dlp gathering is on, send', this.fileName, 'to sandbox:', this.sandboxBundleName, this.appIndex);
+          sortByAuthPerm = true;
+        }
+
+        if (!this.alreadyOpen && !sortByAuthPerm) {
+          hiTraceMeter.startTrace('DlpInstallSandboxJs', startId);
+          let appInfo = await dlpPermission.installDLPSandbox(this.sandboxBundleName,
+            this.authPerm, this.userId, this.uri);
+          this.appIndex = appInfo.appIndex;
+          this.tokenId = appInfo.tokenID;
+        }
+      } catch (err) {
+        console.error(TAG, 'installDLPSandbox failed', err.code, err.message);
+        hiTraceMeter.finishTrace('DlpInstallSandboxJs', startId);
+        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
+        opening = false;
+        await this.sendDlpFileOpenFault(Constants.DLP_INSTALL_SANDBOX_ERROR, this.sandboxBundleName, -1, err.data); // 104:DLP_INSTALL_SANDBOX_ERROR
+        await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_INSIDE_ERROR });
+        await this.closeFile();
+        reject();
+        return;
+      }
+      hiTraceMeter.finishTrace('DlpInstallSandboxJs', startId);
+      await this.sendDlpFileOpenEvent(Constants.DLP_INSTALL_SANDBOX_SUCCESS, this.sandboxBundleName, this.appIndex); // 202: DLP_INSTALL_SANDBOX_SUCCESS
+      if (!this.alreadyOpen) {
+        try {
+          await this.getAlreadyOpen(startId);
+        } catch {
+          reject();
+          return;
+        }
+      }
+      resolve();
+    });
+  }
+  async getAlreadyOpen(startId): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      let timestamp = new Date().getTime();
+      let splitNames = this.fileName.split('.');
+      console.debug(TAG, 'splitNames:', splitNames);
+      if (splitNames.length <= SUFFIX_INDEX) {
+        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
+        opening = false;
+        await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_INSIDE_ERROR });
+        await this.closeFile();
+        reject(); return;
+      }
+      let secondarySuffix = splitNames[splitNames.length - SUFFIX_INDEX];
+      this.linkFileName = this.sandboxBundleName + '_' + this.appIndex + '_' + timestamp + '.' + secondarySuffix + '.dlp.link';
+      hiTraceMeter.startTrace('DlpAddLinkFileJs', startId);
+      try {
+        await this.getAddDLPLinkFile(startId);
+      } catch {
+        reject(); return;
+      }
+      hiTraceMeter.finishTrace('DlpAddLinkFileJs', startId);
+      try {
+        this.linkFilePath = '/mnt/data/fuse/' + this.linkFileName;
+        let stat: fileio.Stat = fileio.statSync(this.linkFilePath);
+        const WRITE_ACCESS: number = 0o0200;
+        this.linkFd = -1;
+        if (stat.mode & WRITE_ACCESS) {
+          this.linkFd = fileio.openSync(this.linkFilePath, 0o2);
+          this.linkFileWriteable = true;
+        } else {
+          this.linkFd = fileio.openSync(this.linkFilePath, 0o0);
+          this.linkFileWriteable = false;
+        }
+      } catch (e) {
+        console.error(TAG, 'file error', e);
+        opening = false;
+        if (this.linkFd !== -1) {
+          fileio.close(this.linkFd);
+        }
+        globalThis.viewContext.terminateSelf();
+      }
+      this.linkUri = getFileUriByPath(this.linkFilePath);
+      if (this.linkUri === '') {
+        console.error(TAG, 'get linkUri ByPath fail', this.linkFilePath);
+        opening = false;
+        globalThis.viewContext.terminateSelf();
+      }
+      resolve();
+    });
+  }
+  async getAddDLPLinkFile(startId): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await this.dlpFile.addDLPLinkFile(this.linkFileName);
+      } catch (error) {
+        console.error(TAG, 'addDLPLinkFile failed', error.code, error.message);
+        try {
+          await this.dlpFile.closeDLPFile();
+        } catch (err) {
+          console.error(TAG, 'closeDLPFile failed', err.code, err.message);
+        }
+        opening = false;
+        await startAlertAbility(globalThis.viewContext, error);
+        hiTraceMeter.finishTrace('DlpAddLinkFileJs', startId);
+        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
+        await this.closeFile();
+        reject();
+        return;
+      }
+      resolve();
+    });
+  }
+  async getUriInfo(startId): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.uriInfo = new fileUri.FileUri(this.uri);
+      } catch (error) {
+        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
+        opening = false;
+        await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_GET_FILE_ASSET_ERROR });
+        await this.closeFile();
+        console.error(TAG, 'open', this.uri, 'failed', error.code, error.message);
+        reject();
+        return;
+      }
+      try {
+        this.stat = await fs.stat(this.uriInfo.path);
+      } catch (err) {
+        console.info(TAG, 'stat fail', err.code, err.message);
+      }
+      resolve();
+    });
+  }
+
   async onRequest(want: Want, startId: number): Promise<void> {
     console.debug(TAG, 'enter onRequest');
     if (opening) {
@@ -249,179 +468,20 @@ export default class ViewAbility extends ServiceExtensionAbility {
       console.error(TAG, 'get parameters failed');
       globalThis.viewContext.terminateSelf();
     }
-    hiTraceMeter.startTrace('DlpGetOsAccountJs', startId);
-    let accountInfo;
     try {
-      accountInfo = await getOsAccountInfo();
-      this.userId = await getUserId();
-    } catch (err) {
-      console.error(TAG, 'getOsAccountInfo failed', err.code, err.message);
-      hiTraceMeter.finishTrace('DlpGetOsAccountJs', startId);
-      hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
-      opening = false;
-      await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_GET_ACCOUNT_ERROR });
-      fileio.closeSync(this.dlpFd);
-      return;
-    }
-    hiTraceMeter.finishTrace('DlpGetOsAccountJs', startId);
-    if (!checkAccountLogin(accountInfo)) {
-      hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
-      opening = false;
-      await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_NO_ACCOUNT_ERROR });
-      fileio.closeSync(this.dlpFd);
-      return;
-    }
-
-    hiTraceMeter.startTrace('DlpOpenDlpFileJs', startId);
-    try {
-      console.info(TAG, 'openDLPFile', this.fileName, this.dlpFd);
-      this.dlpFile = await dlpPermission.openDLPFile(this.dlpFd);
-    } catch (err) {
-      console.error(TAG, 'openDLPFile', this.fileName, 'failed', err.code, err.message);
-      try {
-        hiTraceMeter.finishTrace('DlpOpenDlpFileJs', startId);
-        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
-        opening = false;
-        await this.sendDlpFileOpenFault(Constants.DLP_FILE_PARSE_ERROR, this.sandboxBundleName, -1, err.data); // 103:DLP_FILE_PARSE_ERROR
-        await startAlertAbility(globalThis.viewContext, err);
-      } catch (e) {
-        console.error(TAG, 'openDLPFile2', this.fileName, 'failed', e.code, e.message, e);
-      }
-      fileio.closeSync(this.dlpFd);
-      return;
-    }
-    hiTraceMeter.finishTrace('DlpOpenDlpFileJs', startId);
-    if (globalThis.domainAccount) {
-      this.authPerm = getAuthPerm(accountInfo.domainInfo.accountName, this.dlpFile.dlpProperty);
-    } else {
-      this.authPerm = getAuthPerm(accountInfo.distributedInfo.name, this.dlpFile.dlpProperty);
-    }
-    if (this.authPerm < dlpPermission.DLPFileAccess.READ_ONLY ||
-      this.authPerm > dlpPermission.DLPFileAccess.FULL_CONTROL) {
-      opening = false;
-      await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_INSIDE_ERROR });
-      await this.closeFile();
-      return;
-    }
-
-    let sortByAuthPerm: boolean = false;
-    this.alreadyOpen = false;
-
-    try {
-      this.gatheringType = await dlpPermission.getDLPGatheringPolicy();
-      if (globalThis.fileOpenHistory[this.uri] !== undefined) {
-        console.info(TAG, 'file', this.fileName, 'already open');
-        this.appIndex = globalThis.fileOpenHistory[this.uri][Constants.FILE_OPEN_HISTORY_ONE];
-        this.linkFileName = globalThis.fileOpenHistory[this.uri][Constants.FILE_OPEN_HISTORY_TWO];
-        this.linkFd = globalThis.fileOpenHistory[this.uri][Constants.FILE_OPEN_HISTORY_THREE];
-        this.linkUri = globalThis.fileOpenHistory[this.uri][Constants.FILE_OPEN_HISTORY_FOUR];
-        await this.closeFile();
-        this.alreadyOpen = true;
-      }
-
-      if (globalThis.authPerm2Sandbox[this.authPerm] !== undefined &&
-        this.gatheringType === dlpPermission.GatheringPolicyType.GATHERING) {
-        this.appIndex = globalThis.authPerm2Sandbox[this.authPerm][1];
-        console.info(TAG, 'Dlp gathering is on, send', this.fileName, 'to sandbox:', this.sandboxBundleName, this.appIndex);
-        sortByAuthPerm = true;
-      }
-
-      if (!this.alreadyOpen && !sortByAuthPerm) {
-        hiTraceMeter.startTrace('DlpInstallSandboxJs', startId);
-        let appInfo = await dlpPermission.installDLPSandbox(this.sandboxBundleName,
-          this.authPerm, this.userId, this.uri);
-        this.appIndex = appInfo.appIndex;
-        this.tokenId = appInfo.tokenID;
-      }
-    } catch (err) {
-      console.error(TAG, 'installDLPSandbox failed', err.code, err.message);
-      hiTraceMeter.finishTrace('DlpInstallSandboxJs', startId);
-      hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
-      opening = false;
-      await this.sendDlpFileOpenFault(Constants.DLP_INSTALL_SANDBOX_ERROR, this.sandboxBundleName, -1, err.data); // 104:DLP_INSTALL_SANDBOX_ERROR
-      await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_INSIDE_ERROR });
-      await this.closeFile();
-      return;
-    }
-    hiTraceMeter.finishTrace('DlpInstallSandboxJs', startId);
-    await this.sendDlpFileOpenEvent(Constants.DLP_INSTALL_SANDBOX_SUCCESS, this.sandboxBundleName, this.appIndex); // 202: DLP_INSTALL_SANDBOX_SUCCESS
-    if (!this.alreadyOpen) {
-      let date = new Date();
-      let timestamp = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-        date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getMilliseconds()).getTime();
-      let splitNames = this.fileName.split('.');
-      console.debug(TAG, 'splitNames:', splitNames);
-      if (splitNames.length <= SUFFIX_INDEX) {
-        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
-        opening = false;
-        await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_INSIDE_ERROR });
-        await this.closeFile();
-        return;
-      }
-      let secondarySuffix = splitNames[splitNames.length - SUFFIX_INDEX];
-      this.linkFileName = this.sandboxBundleName + '_' + this.appIndex + '_' + timestamp + '.' + secondarySuffix + '.dlp.link';
-      hiTraceMeter.startTrace('DlpAddLinkFileJs', startId);
-      try {
-        await this.dlpFile.addDLPLinkFile(this.linkFileName);
-      } catch (error) {
-        console.error(TAG, 'addDLPLinkFile failed', error.code, error.message);
-        try {
-          await this.dlpFile.closeDLPFile();
-        } catch (err) {
-          console.error(TAG, 'closeDLPFile failed', err.code, err.message);
-        }
-        opening = false;
-        await startAlertAbility(globalThis.viewContext, error);
-        hiTraceMeter.finishTrace('DlpAddLinkFileJs', startId);
-        hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
-        await this.closeFile();
-        return;
-      }
-      hiTraceMeter.finishTrace('DlpAddLinkFileJs', startId);
-      try {
-        this.linkFilePath = '/mnt/data/fuse/' + this.linkFileName;
-        // @ts-ignore
-        let stat: fileio.Stat = fileio.statSync(this.linkFilePath);
-        const WRITE_ACCESS: number = 0o0200;
-        this.linkFd = -1;
-        if (stat.mode & WRITE_ACCESS) {
-          // @ts-ignore
-          this.linkFd = fileio.openSync(this.linkFilePath, 0o2);
-          this.linkFileWriteable = true;
-        } else {
-          // @ts-ignore
-          this.linkFd = fileio.openSync(this.linkFilePath, 0o0);
-          this.linkFileWriteable = false;
-        }
-      } catch (e) {
-        console.error(TAG, 'file error', e);
-        opening = false;
-        if (this.linkFd !== -1) {
-          fileio.close(this.linkFd);
-        }
-        globalThis.viewContext.terminateSelf();
-      }
-      this.linkUri = getFileUriByPath(this.linkFilePath);
-      if (this.linkUri === '') {
-        console.error(TAG, 'get linkUri ByPath fail', this.linkFilePath);
-        opening = false;
-        globalThis.viewContext.terminateSelf();
-      }
-    }
-    try {
-      this.uriInfo = new fileUri.FileUri(this.uri);
-    } catch (error) {
-      hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
-      opening = false;
-      await startAlertAbility(globalThis.viewContext, { code: Constants.ERR_JS_APP_GET_FILE_ASSET_ERROR });
-      await this.closeFile();
-      console.error(TAG, 'open', this.uri, 'failed', error.code, error.message);
+      await this.getAccountAndOpenDLPFile(startId);
+    } catch {
       return;
     }
     try {
-      this.stat = await fs.stat(this.uriInfo.path);
-    } catch (err) {
-      console.log(TAG, 'stat fail', err.code, err.message);
+      await this.getPolicyAndInstallSandbox(startId);
+    } catch {
+      return;
+    }
+    try {
+      await this.getUriInfo(startId);
+    } catch {
+      return;
     }
     this.startSandboxApp(startId);
     hiTraceMeter.finishTrace('DlpOpenFileJs', startId);
