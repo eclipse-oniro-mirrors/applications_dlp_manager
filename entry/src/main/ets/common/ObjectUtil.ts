acbc45b14187c718c9283a920fba4163a0358233
li-li-wang <wangliliang5@huawei.com>
2024-07-26 10:43:36 +0800

    翻译回稿合入
    Signed-off-by: li-li-wang <wangliliang5@huawei.com>

--- entry/src/main/ets/common/ObjectUtil.ts
+++ entry/src/main/ets/common/ObjectUtil.ts
@@ -14,7 +14,7 @@
  */
 
 export class ObjectUtil {
-  static Assign<T extends {}, U>(target: T, source: U): T & U{
+  static Assign<T extends {}, U>(target: T, source: U): T & U {
     return Object.assign(target, source);
   }
 }
