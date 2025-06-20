1c7d09ec9f6796fad3b6144f4597d19f5de59777
0202zc <lzc980202@163.com>
2025-06-20 04:43:37 +0000

    codecheck修复
    Signed-off-by: 0202zc <lzc980202@163.com>

--- entry/src/main/ets/bean/data/AccountAssociationCacheDTO.ts
+++ entry/src/main/ets/bean/data/AccountAssociationCacheDTO.ts
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { AuthorizedAccount } from "./AuthorizedAccount";
+import { AuthorizedAccount } from './AuthorizedAccount';
 
 export default class AccountAssociationCacheDTO {
   private accountId: string;
