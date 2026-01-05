e788395c8190aed626b228b94be775bf8df12d08
0202zc <lzc980202@163.com>
2026-01-05 15:06:19 +0800

    新增DTTest
    Signed-off-by: 0202zc <lzc980202@163.com>

--- entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
+++ entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import abilityDelegatorRegistry from '@ohos.app.ability.abilityDelegatorRegistry';
+import AbilityDelegatorRegistry from '@ohos.app.ability.abilityDelegatorRegistry';
 import hilog from '@ohos.hilog';
 import TestRunner from '@ohos.application.testRunner';
 
@@ -27,7 +27,7 @@ async function onAbilityCreateCallback(): Promise<void> {
 }
 
 async function addAbilityMonitorCallback(err): Promise<void> {
-  hilog.info(0x0000, 'testTag', '%{public}s', 'addAbilityMonitorCallback : %{public}s', JSON.stringify(err) ?? '');
+  hilog.info(0x0000, 'testTag', 'addAbilityMonitorCallback : %{public}s', JSON.stringify(err) ?? '');
 }
 
 export default class OpenHarmonyTestRunner implements TestRunner {
@@ -39,9 +39,9 @@ export default class OpenHarmonyTestRunner implements TestRunner {
   }
 
   async onRun(): Promise<void> {
-    hilog.info(0x0000, 'DlpManager', '%{public}s', 'DlpManager OnRun');
-    abilityDelegatorArguments = abilityDelegatorRegistry.getArguments();
-    abilityDelegator = abilityDelegatorRegistry.getAbilityDelegator();
+    hilog.info(0x0000, 'DlpManager', '%{public}s', 'DlpManager OnRun start');
+    abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments();
+    abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator();
 
     const savePath: string = '__savePath__';
     const readPath: string = '__readPath__';
@@ -49,7 +49,7 @@ export default class OpenHarmonyTestRunner implements TestRunner {
     let uid: number = Math.floor(abilityDelegator.getAppContext().applicationInfo.uid / 200000);
     const bundleName: string = abilityDelegatorArguments.bundleName;
     global[savePath] = '/data/storage/el2/base/js_coverage.json';
-    global[readPath] = '/data/app/el2/' + uid + '/base/' + bundleName + 'js_coverage.json';
+    global[readPath] = '/data/app/el2/' + uid + '/base/' + bundleName + '/js_coverage.json';
     global[testMode] = 'ohostest';
 
     let abilityName = abilityDelegatorArguments.bundleName + '.TestAbility';
@@ -70,6 +70,6 @@ export default class OpenHarmonyTestRunner implements TestRunner {
         hilog.info(0x0000, 'DlpManager', 'executeShellCommand : data : %{public}s', d.stdResult ?? '');
         hilog.info(0x0000, 'DlpManager', 'executeShellCommand : data : %{public}s', d.exitCode ?? '');
       });
-    hilog.info(0x0000, 'DlpManager', '%{public}s', 'DlpManager OnRun end');
+    hilog.info(0x0000, 'DlpManager', '%{public}s', 'OpenHarmonyTestRunner OnRun end');
   }
 }
